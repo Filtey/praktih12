451c9beac5645294e22e0218c4f0242f9551c012
Erik Khakimov <erik89826502855@gmail.com>
2022-01-18 01:27:27 +0500

    podpravilDelenie, a ne umnogenie

--- laba12program/Program.cs
+++ laba12program/Program.cs
@@ -13,15 +13,11 @@ namespace laba12program
             Console.WriteLine("Ввести А\nВвести В\nвыполнить операцию '+'\nВыполнить операцию '-'\nВыполнить операцию '*'\nВыполнить операцию '/'\n");
             string menu = Console.ReadLine();
             int a = 0, b = 0;
-<<<<<<< HEAD
-            if (menu == "Ввести В")
-            {
-                b = int.Parse(Console.ReadLine());
-=======
+
             if(menu == "Выполнить операцию '/'")
             {
                 Console.WriteLine(a/b);
->>>>>>> delenie
+
             }
         }
     }
