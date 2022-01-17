baf0aebb4611ef0fe511c43c5adedb45a810f2a9
Erik Khakimov <erik89826502855@gmail.com>
2022-01-18 01:04:46 +0500

    nahalo

---
+++ laba12program/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba12program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввести А\nВвести В\nвыполнить операцию '+'\nВыполнить операцию '-'\nВыполнить операцию '*'\nВыполнить операцию '/'\n");
+        }
+    }
+}
